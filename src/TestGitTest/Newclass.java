11464819b3c3353618504237fdb37f6d24d33db7
ghalib <syedghalib785@gmail.com>
2022-12-09 14:02:55 +0530

    1st push to git

---
+++ src/TestGitTest/Newclass.java
@@ -0,0 +1,12 @@
+package TestGitTest;
+
+public class Newclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(""""""""""""""""""");
+
+	}
+
+}
